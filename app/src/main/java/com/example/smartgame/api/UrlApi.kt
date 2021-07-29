a6c43fd00e901e997baa28b9fdead67d5a4eb885
Lorrana Santos <67704928+lorranasantos@users.noreply.github.com>
2021-07-29 14:30:39 -0300

    Update UrlApi.kt

--- app/src/main/java/com/example/smartgame/api/UrlApi.kt
+++ app/src/main/java/com/example/smartgame/api/UrlApi.kt
@@ -2,6 +2,6 @@ package com.example.smartgameapp.api
 
 class UrlApi {
     companion object{
-        const val BASE_URL = "http://192.168.100.6:3333/"
+        const val BASE_URL = "http://localhost:3333/"
     }
 }
